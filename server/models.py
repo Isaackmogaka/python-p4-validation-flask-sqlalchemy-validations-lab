7e5f7b4246322dce5e6df310b84eabb4d6e2b935
isaacmogaka <isaacmogaka2016@gmail.com>
2023-09-21 21:24:40 +0300

    Completed Assignment

--- server/models.py
+++ server/models.py
@@ -1,23 +1,64 @@
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy.orm import validates
+# db = SQLAlchemy()
+
+# class Author(db.Model):
+#     __tablename__ = 'authors'
+#     # Add validations and constraints 
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name= db.Column(db.String, unique=True, nullable=False)
+#     phone_number = db.Column(db.String)
+#     created_at = db.Column(db.DateTime, server_default=db.func.now())
+#     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+#     def __repr__(self):
+#         return f'Author(id={self.id}, name={self.name})'
+
+# class Post(db.Model):
+#     __tablename__ = 'posts'
+#     # Add validations and constraints 
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String, nullable=False)
+#     content = db.Column(db.String)
+#     category = db.Column(db.String)
+#     summary = db.Column(db.String)
+#     created_at = db.Column(db.DateTime, server_default=db.func.now())
+#     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+
+#     def __repr__(self):
+#         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 db = SQLAlchemy()
 
 class Author(db.Model):
     __tablename__ = 'authors'
-    # Add validations and constraints 
 
     id = db.Column(db.Integer, primary_key=True)
-    name= db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String, unique=True, nullable=False)
     phone_number = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    def __repr__(self):
-        return f'Author(id={self.id}, name={self.name})'
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Author name cannot be empty")
+        return name
+
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if phone_number and len(phone_number) != 10:
+            raise ValueError("Author phone number must be exactly ten digits")
+        return phone_number
 
 class Post(db.Model):
     __tablename__ = 'posts'
-    # Add validations and constraints 
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
@@ -27,6 +68,27 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError("Post title cannot be empty")
+        return title
 
-    def __repr__(self):
-        return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+    @validates('content')
+    def validate_content(self, key, content):
+        if content and len(content) < 250:
+            raise ValueError("Post content must be at least 250 characters long")
+        return content
+
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if summary and len(summary) > 250:
+            raise ValueError("Post summary cannot be longer than 250 characters")
+        return summary
+
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in ['Fiction', 'Non-Fiction']:
+            raise ValueError("Post category must be 'Fiction' or 'Non-Fiction'")
+        return category
+
